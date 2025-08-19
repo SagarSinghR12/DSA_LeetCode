19ae358abce3ee826021d08fc681aad8e6be7919
SagarSinghR12 <131946657+SagarSinghR12@users.noreply.github.com>
2025-08-19 16:41:02 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (97.54%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        // Dutch National Flag  ->  DNF Algorithm
+        int i = 0;
+        int j = 0;
+        int k = nums.size()-1;
+        while(j <= k){
+            if(nums[j] == 0)
+                swap(nums[i++], nums[j++]);
+            else if(nums[j] == 1)
+                j++;
+            else{
+                swap(nums[j], nums[k]);
+                k--;
+            }
+        }
+    }
+};
