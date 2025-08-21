7d8e45df58e0a84b7fd9b5691f624824e8869674
SagarSinghR12 <131946657+SagarSinghR12@users.noreply.github.com>
2025-08-21 15:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (22.97%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        vector<int> count(n+1, 0);
+
+        // Populate the count array:
+        for(int i : citations){
+            if(i >= n)
+                count[n]++;
+            else
+                count[i]++;
+        }
+
+        // Calculate the H-Index:
+        int total = 0;
+        for(int i=n; i>=0; i--){
+            total += count[i];
+            if(total >= i)
+                return i;
+        }
+
+        return 0;
+    }
+};
