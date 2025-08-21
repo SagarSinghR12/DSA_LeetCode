a0583dce8f5261dccd9b96d913970daf6fb3ae83
SagarSinghR12 <131946657+SagarSinghR12@users.noreply.github.com>
2025-08-21 15:13:33 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (55.26%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/reshape-the-matrix.cpp
@@ -18,8 +18,26 @@ public:
         return newMat;
     }
 
+    vector<vector<int>> method_2(vector<vector<int>>& mat, int r, int c) {
+        int m = mat.size();
+        int n = mat[0].size();
+        if(m*n != r*c)
+            return mat;
+
+        vector<vector<int>> newMat(r, vector<int>(c));
+        
+        for(int i=0; i<m*n; i++){
+            newMat[i/c][i%c] = mat[i/n][i%n];
+        }
+
+        return newMat;
+    }
+
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        // Method-1;
-        return method_1(mat, r, c);
+        // Method-1:
+        // return method_1(mat, r, c);
+
+        // Method-2:
+        return method_2(mat, r, c);
     }
 };
