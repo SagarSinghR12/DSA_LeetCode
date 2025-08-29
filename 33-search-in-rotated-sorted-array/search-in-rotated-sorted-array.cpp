63c0f2829da6c2dd60718fc53d7c77d9b1a26004
SagarSinghR12 <131946657+SagarSinghR12@users.noreply.github.com>
2025-08-29 20:31:58 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.09%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     int pivotIndex(vector<int> &nums){
         int start = 0;
         int end = nums.size()-1;
@@ -42,5 +43,35 @@ public:
             ans = binarySearch(nums, pivot+1, nums.size()-1, target);
         
         return ans;
+    }
+    */
+
+    int search(vector<int>& nums, int target){
+        int start = 0;
+        int end = nums.size()-1;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] == target)
+                return mid;
+
+            // search in Left
+            if(nums[start]<=nums[mid]){
+                if(target>=nums[start] && target<nums[mid])
+                    end = mid-1;
+                else
+                    start = mid+1;
+            }
+            // Search in Right
+            else{
+                if(target>nums[mid] && target<=nums[end])
+                    start = mid+1;
+                else 
+                    end = mid-1;
+            }
+        }
+
+        return -1;
     }
 };
