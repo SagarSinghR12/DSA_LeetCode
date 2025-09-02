5d81e5c64aebdb022c1b17bb48f603e0f08c9bcf
SagarSinghR12 <131946657+SagarSinghR12@users.noreply.github.com>
2025-09-02 15:10:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (15.30%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,11 +1,44 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        int pos = s.find(part);
-        while(pos != string::npos){
-            s.erase(pos, part.length());
-            pos = s.find(part);
+
+    string removeOccurrences_M(string s, string part) {
+        string ans = "";
+        int n = part.length();
+
+        for (char c : s) {
+            ans.push_back(c);  // add current character
+
+            // check if ans ends with 'part'
+            if (ans.size() >= n) {
+                bool match = true;
+                for (int i = 0; i < n; i++) {
+                    if (ans[ans.size() - n + i] != part[i]) {
+                        match = false;
+                        break;
+                    }
+                }
+
+                // if found, remove last n characters
+                if (match) {
+                    for (int i = 0; i < n; i++) {
+                        ans.pop_back();
+                    }
+                }
+            }
         }
-        return s;
+
+        return ans;
+    }
+
+    string removeOccurrences(string s, string part) {
+        // int pos = s.find(part);
+        // while(pos != string::npos){
+        //     s.erase(pos, part.length());
+        //     pos = s.find(part);
+        // }
+        // return s;
+
+        string ans = removeOccurrences_M(s, part);
+        return ans;
     }
 };
