cfdf5230101d541e0f85a38ad3305eda44077daa
SagarSinghR12 <131946657+SagarSinghR12@users.noreply.github.com>
2025-09-05 11:32:28 +0530

    Time: 116 ms (47.21%) | Memory: 8.9 MB (91.70%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int beautySum(string s) {
         int beautySum = 0;
-        unordered_map<char, int> mp;
+        // unordered_map<char, int> mp;
 
         // Find all substrings
         for(int i=0; i<s.size(); i++){
-            mp.clear();
+            // mp.clear();
+            int mp[256] = {0};       //256->ASCII chars, we'll store fre of 26 chars.
             for(int j=i; j<s.size(); j++){
                 // char ch = s[j];
                 char &ch = s[j];
@@ -15,9 +16,16 @@ public:
                 int mini = INT_MAX;
                 int maxi = INT_MIN;
 
-                for(auto i : mp){
-                    maxi = max(maxi, i.second);
-                    mini = min(mini, i.second);
+                // for(auto i : mp){
+                //     maxi = max(maxi, i.second);
+                //     mini = min(mini, i.second);
+                // }
+
+                for(char ch = 'a'; ch <= 'z'; ch++){
+                    if(mp[ch] > 0){
+                        maxi = max(maxi, mp[ch]);
+                        mini = min(mini, mp[ch]);
+                    }
                 }
 
                 int beauty = maxi - mini;
