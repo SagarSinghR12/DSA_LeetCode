67ad6709de40cd3d7b1212f1449bfb8f598d42c8
SagarSinghR12 <131946657+SagarSinghR12@users.noreply.github.com>
2025-09-24 09:09:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (95.40%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int hash[26] = {0};
+
+        for(int i=0; i<s.size(); i++){
+            hash[s[i]-'a']++;
+        } //O(n)
+
+        // Find the most frequent character
+        char max_freq_char;
+        int max_freq = INT_MIN;
+        for(int i=0; i<26; i++){
+            if(hash[i] > max_freq){
+                max_freq = hash[i];
+                max_freq_char = i + 'a';
+            }
+        }
+
+        // Placing most frequent character
+        int index = 0;
+        while(max_freq > 0  &&  index < s.size()){
+            s[index] = max_freq_char;
+            max_freq--;
+            index += 2;
+        } //O(n)
+
+        // Exit condition --> if max_freq_char is not placed
+        if(max_freq != 0)
+            return "";
+
+        hash[max_freq_char - 'a'] = 0;
+
+        // Let's place the rest of the characters
+        for(int i=0; i<26; i++){
+            while(hash[i]>0){
+                index = (index >= s.size()) ? 1 : index;
+                s[index] = i + 'a';
+                hash[i]--;
+                index += 2;
+            }
+        } //O(26*n) -> O(n)
+
+        return s;
+    }
+};
