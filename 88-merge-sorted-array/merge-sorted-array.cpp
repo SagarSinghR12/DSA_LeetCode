5c53b03b4e19dce49ec268ac4a66a8b6a73ce960
SagarSinghR12 <131946657+SagarSinghR12@users.noreply.github.com>
2025-10-05 21:17:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (39.33%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    void merge_stdAprch(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         vector<int> ans;
         int i = 0;
         int j= 0;
@@ -20,5 +20,24 @@ public:
 
         for(int k=0; k<ans.size(); k++)
             nums1[k] = ans[k];
+    }
+
+    void merge_inPlace(vector<int>& nums1, int m, vector<int>& nums2, int n){
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i] > nums2[j])
+                nums1[k--] = nums1[i--];
+            else
+                nums1[k--] = nums2[j--];
+        }
+        while(j>=0)
+            nums1[k--] = nums2[j--];
+    }
+
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // merge_stdAprch(nums1, m, nums2, n);
+        merge_inPlace(nums1, m, nums2, n);
     }
 };
