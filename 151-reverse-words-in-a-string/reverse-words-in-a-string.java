39d83011fd3fbfef40f493b6c26893c6d64b596e
SagarSinghR12 <131946657+SagarSinghR12@users.noreply.github.com>
2026-03-08 18:36:20 +0530

    Time: 4 ms (93.72%) | Memory: 44.3 MB (79.73%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,6 +1,5 @@
 class Solution {
-    public String reverseWords(String s) {
-        s = s.trim();
+    public String method1(String s) {
         String[] str = s.split("\\s+");
         StringBuilder sb = new StringBuilder();
 
@@ -11,5 +10,34 @@ class Solution {
         }
 
         return sb.toString();
+    }
+
+    public String method2(String s) {
+        StringBuilder sb = new StringBuilder();
+        int i = s.length() - 1;
+
+        while(i >= 0) {
+            while(i>=0 && s.charAt(i)==' ')
+                i--;
+            
+            if(i < 0)
+                break;
+            
+            int j = i;
+
+            while(i>=0 && s.charAt(i)!=' ')
+                i--;
+
+            sb.append(s.substring(i+1, j+1)).append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+
+    public String reverseWords(String s) {
+        s = s.trim();
+        // String str = method1(s);
+        String str = method2(s);
+        return str;
     }
 }
